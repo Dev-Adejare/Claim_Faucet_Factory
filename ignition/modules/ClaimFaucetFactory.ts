f4ad24a5a202ce1ff8c92742979be3b5e6c5a86a
root <root@ADEJARE-02>
2024-10-17 15:52:38 +0100

    update

---
+++ ignition/modules/ClaimFaucetFactory.ts
@@ -0,0 +1,9 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const ClaimFaucetFactoryModule = buildModule("ClaimFaucetFactoryModule", (m) => {
+  const claimFaucetFactory = m.contract("ClaimFaucetFactory");
+
+  return { claimFaucetFactory };
+});
+
+export default ClaimFaucetFactoryModule;
